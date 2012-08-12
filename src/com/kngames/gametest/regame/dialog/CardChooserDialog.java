406dd7c84799ce772f0f67231d9505723d5f2da9
Konnor Nicolau <konnornicolau@gmail.com>
2012-08-12 03:55:21 -0700

    renamed package regame.Dialog

---
+++ src/com/kngames/gametest/regame/dialog/CardChooserDialog.java
@@ -0,0 +1,140 @@
+package com.kngames.gametest.regame.dialog;
+
+import com.kngames.gametest.R;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+
+public class CardChooserDialog extends Dialog {
+
+	protected HorizontalScrollView scrollLayout;
+	protected LinearLayout rowContainer;
+	protected LinearLayout row1;
+	protected LinearLayout row2;
+	protected LinearLayout row3;
+	protected int numButtons = 0;
+	
+	protected Context context;
+	
+	public final int CARD_PIC_WIDTH = 125;
+	public final int CARD_PIC_HEIGHT = 175;
+	public final int SPACER_WIDTH = 16;
+	public final int SPACER_HEIGHT = 16;
+	
+	// the interface for what you want to do on the calling activity
+    public interface DialogEventListener {
+        public void buttonPressed(int value);
+    }
+    DialogEventListener onButtonPressedListener;
+	
+    
+	public CardChooserDialog(Context context, DialogEventListener onCustomDialogEventListener) {
+		super(context);
+		this.onButtonPressedListener = onCustomDialogEventListener;
+		init(context);
+	}
+
+	public CardChooserDialog(Context context, int theme, DialogEventListener onCustomDialogEventListener) {
+		super(context, theme);
+		this.onButtonPressedListener = onCustomDialogEventListener;
+		init(context);
+	}
+
+	public CardChooserDialog(Context context, boolean cancelable,
+			OnCancelListener cancelListener, DialogEventListener onCustomDialogEventListener) {
+		super(context, cancelable, cancelListener);
+		this.onButtonPressedListener = onCustomDialogEventListener;
+		init(context);
+	}
+	
+	//	initialize the structure
+	void init(Context context) {
+		this.context = context;
+    	setTitle("Choose a Card");
+    	setCancelable(true);
+		
+    	setupLayout();
+    	
+    	LinearLayout.LayoutParams mainParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+    	setContentView(scrollLayout, mainParams);
+	}
+	
+	//	set up the layout, short of setting it to the content view
+	void setupLayout() {
+		//	set up mainLayout and rowContainer which will contain all elements
+    	scrollLayout = new HorizontalScrollView(context);
+    	
+    	rowContainer = new LinearLayout(context);
+    	rowContainer.setOrientation(LinearLayout.VERTICAL);
+    	LinearLayout.LayoutParams subParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+    	rowContainer.setLayoutParams(subParams);
+    	
+    	scrollLayout.addView(rowContainer);
+    	
+    	
+    	//	set up structures for rows of card buttons
+		row1 = new LinearLayout(context);
+    	row2 = new LinearLayout(context);
+    	row3 = new LinearLayout(context);
+    	
+    	//	set params for each row
+    	LinearLayout.LayoutParams rowParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, CARD_PIC_HEIGHT);
+    	rowParams.setMargins(SPACER_WIDTH, 0, SPACER_WIDTH, 0);
+    	row1.setLayoutParams(rowParams);
+    	
+    	rowParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, CARD_PIC_HEIGHT);
+    	rowParams.setMargins(SPACER_WIDTH, SPACER_HEIGHT, SPACER_WIDTH, 0);
+    	row2.setLayoutParams(rowParams);
+    	
+    	rowParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, CARD_PIC_HEIGHT);
+    	rowParams.setMargins(SPACER_WIDTH, SPACER_HEIGHT, SPACER_WIDTH, SPACER_HEIGHT);
+    	row3.setLayoutParams(rowParams);
+    	
+    	//	add the rows to the rowContainer, then set the content view
+    	rowContainer.addView(row1);
+    	rowContainer.addView(row2);
+    	rowContainer.addView(row3);
+	}
+	
+	public void addButton(String text) {
+		//	create the new button
+		Button cardButton = new Button(context);
+		cardButton.setId(numButtons);
+    	cardButton.setLayoutParams(new ViewGroup.LayoutParams(CARD_PIC_WIDTH, ViewGroup.LayoutParams.MATCH_PARENT));
+    	cardButton.setText(text);
+    	cardButton.setTextColor(Color.WHITE);
+    	cardButton.setBackgroundResource(R.drawable.card_back_small);
+    	cardButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	onButtonPressedListener.buttonPressed(v.getId());
+            	dismiss();
+            }
+        });
+    	
+    	View spacer = new View(context);
+    	spacer.setLayoutParams(new ViewGroup.LayoutParams(SPACER_WIDTH, SPACER_HEIGHT));
+    	
+		if (numButtons % 3 == 0) {	// add button to top row
+			if (numButtons / 3 != 0) row1.addView(spacer);
+			row1.addView(cardButton);
+		} else if (numButtons % 3 == 1) {	// add button to middle row
+			if (numButtons / 3 != 0) row2.addView(spacer);
+			row2.addView(cardButton);
+		} else {	// add button to bottom row
+			if (numButtons / 3 != 0) row3.addView(spacer);
+			row3.addView(cardButton);
+		}
+		
+		numButtons++;
+	}
+}
---
+++ src/com/kngames/gametest/regame/dialog/ScenarioChooser.java
@@ -0,0 +1,143 @@
+package com.kngames.gametest.regame.dialog;
+
+import java.util.ArrayList;
+
+import com.kngames.gametest.redata.ScenData;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.util.Pair;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.RelativeLayout;
+
+public class ScenarioChooser extends Dialog {
+
+	protected RelativeLayout container;
+	protected ListView scenList;
+	protected LinearLayout buttonPanel;
+	protected Button btnNewScen;
+	protected Button btnCancel;
+	
+	protected Context context;
+	protected ArrayList<Pair<Integer,String>> scenarioPairs;
+	protected ArrayList<String> scenarioStrings;
+	
+	// the interface for what you want to do on the calling activity
+    public interface DialogEventListener {
+        public void buttonPressed(int value);
+    }
+    DialogEventListener onButtonPressedListener;
+	
+    
+	public ScenarioChooser(Context context, DialogEventListener dialogEventListener) {
+		super(context);
+		this.onButtonPressedListener = dialogEventListener;
+		init(context);
+	}
+
+	public ScenarioChooser(Context context, int theme, DialogEventListener onCustomDialogEventListener) {
+		super(context, theme);
+		this.onButtonPressedListener = onCustomDialogEventListener;
+		init(context);
+	}
+
+	public ScenarioChooser(Context context, boolean cancelable,
+			OnCancelListener cancelListener, DialogEventListener onCustomDialogEventListener) {
+		super(context, cancelable, cancelListener);
+		this.onButtonPressedListener = onCustomDialogEventListener;
+		init(context);
+	}
+	
+	//	initialize the structure
+	void init(Context context) {
+		this.context = context;
+    	setTitle("Choose a Scenario");
+    	setCancelable(true);
+		
+    	setupLayout();
+    	
+    	LinearLayout.LayoutParams mainParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+    	setContentView(container, mainParams);
+	}
+	
+	//	set up the layout, short of setting it to the content view
+	void setupLayout() {
+		//	set up main elements
+    	container = new RelativeLayout(context);
+    	RelativeLayout.LayoutParams subParams = new RelativeLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+    	container.setLayoutParams(subParams);
+    	
+    	//  set up the buttons
+    	buttonPanel = new LinearLayout(context);
+    	buttonPanel.setId(9000);
+    	subParams = new RelativeLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+    	subParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+    	buttonPanel.setLayoutParams(subParams);
+    	
+    	btnNewScen = new Button(context);
+    	subParams = new RelativeLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+    	btnNewScen.setLayoutParams(subParams);
+    	btnNewScen.setText("New Scenario");
+    	btnNewScen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	onButtonPressedListener.buttonPressed(-1);
+            	dismiss();
+            }
+        });
+    	buttonPanel.addView(btnNewScen);
+    	
+    	btnCancel = new Button(context);
+    	btnCancel.setLayoutParams(subParams);
+    	btnCancel.setText("Cancel");
+    	btnCancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	dismiss();
+            }
+        });
+    	buttonPanel.addView(btnCancel);
+    	container.addView(buttonPanel);
+    	
+    	//	set up ListView
+    	scenList = new ListView(context);
+    	subParams = new RelativeLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+    	subParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+    	subParams.addRule(RelativeLayout.ABOVE, buttonPanel.getId());
+    	scenList.setLayoutParams(subParams);
+
+    	//	set up custom scenario list
+    	scenarioPairs = new ArrayList<Pair<Integer,String>>();
+    	scenarioStrings = new ArrayList<String>();
+    	for (int i = 0; i < ScenData.CustomScenarios.size(); i++) {
+    		scenarioPairs.add(new Pair<Integer, String>(
+    				ScenData.CustomScenarios.get(i).first, 
+    				ScenData.CustomScenarios.get(i).second.getName()));
+    		scenarioStrings.add(ScenData.CustomScenarios.get(i).second.getName());
+    	}
+    	int resID = android.R.layout.simple_list_item_1;
+    	ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(context, resID, scenarioStrings);
+		scenList.setAdapter(listAdapter);
+		scenList.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> aV, View v, int index, long l) {
+				onButtonPressedListener.buttonPressed(index);
+			}
+		});
+    	
+    	container.addView(scenList);
+	}
+}
