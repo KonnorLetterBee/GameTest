8a8a0a71bbcef5dfc381098e1177ce69789bb2c6
Konnor Nicolau <konnornicolau@gmail.com>
2012-08-12 23:18:27 -0700

    game setup layout update
    Made the game setup screen partially programmatic, using a series of
    player panels that can be turned on and off to match the correct number
    of players
    Added all graphical functionality, everything short of actually getting
    the proper data needed to start a game

--- src/com/kngames/gametest/regame/GameSetupActivity.java
+++ src/com/kngames/gametest/regame/GameSetupActivity.java
@@ -3,61 +3,72 @@ package com.kngames.gametest.regame;
 import java.util.ArrayList;
 
 import com.kngames.gametest.R;
-import com.kngames.gametest.redata.CardData;
 import com.kngames.gametest.redata.ScenData;
 import com.kngames.gametest.redata.Scenario;
-import com.kngames.gametest.redata.CardTypes.CharacterCard;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Spinner;
 
 public class GameSetupActivity extends Activity {
 	
 	private Activity ins = this;
-	private ArrayList<Pair<String,CharacterCard>> charData = getCharacterCards();
-	private String[] charNameList = getNameList(charData);
 	private String[] charChoiceList = new String[] { "Random", "Choose Between Two", "Player Choice" };
 	private String[] playerNumList = new String[] { "1", "2", "3", "4" };
 	private ArrayList<Pair<String,Scenario>> scenData = getScenarios();
 	private String[] scenNameList = getScenarioNameList(scenData);
 	private String[] gameTypeList = ScenData.GameModeString;
 	
+	private GameSetupView playerPanel;
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.game_setup_layout);
+		//setContentView(R.layout.game_setup_layout);
+		ScrollView scroll = (ScrollView) getLayoutInflater().inflate(R.layout.game_setup_layout, null, false);
+		LinearLayout v = (LinearLayout) scroll.findViewById(R.id.container);
+		setContentView(scroll);
+		
+		//	the panel for each individual player to select their player
+		playerPanel = (GameSetupView)findViewById(R.id.game_setup);
+		playerPanel.setNumPlayers(1);
+		playerPanel.setChoice(false);
 		
 		//	spinners
-		Spinner numPlayers = (Spinner)findViewById(R.id.player_spinner);
-		Spinner charChoice = (Spinner)findViewById(R.id.char_select_spinner);
-		Spinner player1 = (Spinner)findViewById(R.id.player_1_spinner);
-		Spinner player2 = (Spinner)findViewById(R.id.player_2_spinner);
-		Spinner player3 = (Spinner)findViewById(R.id.player_3_spinner);
-		Spinner player4 = (Spinner)findViewById(R.id.player_4_spinner);
-		Spinner scenario = (Spinner)findViewById(R.id.scenario_spinner);
-		Spinner gameType = (Spinner)findViewById(R.id.gametype_spinner);
+		Spinner numPlayers = (Spinner)v.findViewById(R.id.player_spinner);
+		Spinner charChoice = (Spinner)v.findViewById(R.id.char_select_spinner);
+		Spinner scenario = (Spinner)v.findViewById(R.id.scenario_spinner);
+		Spinner gameType = (Spinner)v.findViewById(R.id.gametype_spinner);
 		
 		//	spinner adapters
 		ArrayAdapter<String> characterChoiceAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, charChoiceList);
 	    characterChoiceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 	    charChoice.setAdapter(characterChoiceAdapter);
+	    charChoice.setOnItemSelectedListener(new OnItemSelectedListener() {
+			public void onItemSelected(AdapterView<?> arg0, View arg1, int index, long arg3) {
+				if (index == 2) playerPanel.setChoice(true);
+				else playerPanel.setChoice(false);
+			}
+			public void onNothingSelected(AdapterView<?> arg0) { }
+		});
 		
 		ArrayAdapter<String> numPlayerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, playerNumList);
 	    numPlayerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		numPlayers.setAdapter(numPlayerAdapter);
-	    
-		ArrayAdapter<String> playerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, charNameList);
-	    playerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-	    player1.setAdapter(playerAdapter);
-	    player2.setAdapter(playerAdapter);
-	    player3.setAdapter(playerAdapter);
-	    player4.setAdapter(playerAdapter);
+		numPlayers.setOnItemSelectedListener(new OnItemSelectedListener() {
+			public void onItemSelected(AdapterView<?> arg0, View arg1, int index, long arg3) {
+				playerPanel.setNumPlayers(index + 1);
+			}
+			public void onNothingSelected(AdapterView<?> arg0) { }
+		});
 	    
 	    ArrayAdapter<String> scenarioAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, scenNameList);
 	    scenarioAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -67,40 +78,13 @@ public class GameSetupActivity extends Activity {
 	    gameTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 	    gameType.setAdapter(gameTypeAdapter);
 		
-	    //	check boxes
-		CheckBox player1Comp = (CheckBox)findViewById(R.id.player_1_compbox);
-		CheckBox player2Comp = (CheckBox)findViewById(R.id.player_2_compbox);
-		CheckBox player3Comp = (CheckBox)findViewById(R.id.player_3_compbox);
-		CheckBox player4Comp = (CheckBox)findViewById(R.id.player_4_compbox);
-		player1Comp.setTextSize(10);
-		player2Comp.setTextSize(10);
-		player3Comp.setTextSize(10);
-		player4Comp.setTextSize(10);
-		
 		//	start button
-		Button startButton = (Button)findViewById(R.id.startButton);
+		Button startButton = (Button)v.findViewById(R.id.startButton);
 		startButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				ins.finish();
 			}
 		});
-	}
-	
-	private ArrayList<Pair<String,CharacterCard>> getCharacterCards() {
-		ArrayList<Pair<String,CharacterCard>> data = new ArrayList<Pair<String,CharacterCard>>();
-		for (int i = 0; i < CardData.Characters.length; i++) {
-			CharacterCard temp = CardData.Characters[i];
-			data.add(new Pair<String, CharacterCard>(String.format("%s (%s)", temp.getName(), CardData.expansString(temp.getExpansion())), temp));
-		}
-		return data;
-	}
-	
-	private String[] getNameList(ArrayList<Pair<String,CharacterCard>> pairs) {
-		String[] data = new String[pairs.size()];
-		for (int i = 0; i < pairs.size(); i++) {
-			data[i] = pairs.get(i).first;
-		}
-		return data;
 	}
 	
 	private ArrayList<Pair<String,Scenario>> getScenarios() {
--- src/com/kngames/gametest/regame/GameSetupView.java
+++ src/com/kngames/gametest/regame/GameSetupView.java
@@ -1,64 +1,178 @@
 package com.kngames.gametest.regame;
 
-import android.content.Context;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
-import android.widget.LinearLayout;
+import java.util.ArrayList;
 
-public class GameSetupView extends ScrollView {
+import com.kngames.gametest.redata.CardData;
+import com.kngames.gametest.redata.CardTypes.CharacterCard;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Pair;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class GameSetupView extends LinearLayout {
 	
-	protected RelativeLayout relShell;
-	protected RelativeLayout relLayout;
-	protected LinearLayout buttonContainer;
-	
-	public final int TEXT_SIZE = 30;
-	public final int BUTTON_WIDTH = 400;
-	public final int BUTTON_HEIGHT = 150;
-	public final int SPACER_WIDTH = 16;
-	public final int SPACER_HEIGHT = 16;
+	private ArrayList<Pair<String,CharacterCard>> charData = getCharacterCards();
+	private String[] charNameList = getNameList(charData);
 	
 	protected Context context;
-	protected MainMenu parent;
 	
-	public GameSetupView(Context context, MainMenu parent) {
+	private class PlayerEntry extends RelativeLayout {
+		TextView label;
+		CheckBox computer;
+		Spinner selector;
+		
+		private boolean selectorActive = true;
+		
+		private RelativeLayout.LayoutParams selOnLabelParams() {
+			RelativeLayout.LayoutParams labelParams = new RelativeLayout.LayoutParams(
+					RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+		    labelParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+		    labelParams.addRule(RelativeLayout.ALIGN_BOTTOM, computer.getId());
+		    return labelParams;
+		}
+		
+		private RelativeLayout.LayoutParams selOffLabelParams() {
+			RelativeLayout.LayoutParams labelParams = new RelativeLayout.LayoutParams(
+					RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+		    labelParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+		    labelParams.addRule(RelativeLayout.ALIGN_BASELINE, computer.getId());
+		    return labelParams;
+		}
+		
+		public PlayerEntry(int playerNum) {
+			super(context);
+			
+			//	initialize components and set text
+			label = new TextView(context);
+			label.setId(1000);
+			computer = new CheckBox(context);
+			computer.setId(1001);
+			selector = new Spinner(context);
+			selector.setId(1002);
+			
+			label.setText("Player " + playerNum);
+			computer.setText("CPU player");
+			
+			//	set adapter for choosing character
+			ArrayAdapter<String> playerAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, charNameList);
+		    playerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		    selector.setAdapter(playerAdapter);
+		    
+		    //	LayoutParams for the label
+		    this.addView(label, selOnLabelParams());
+		    
+		    //	LayoutParams for the checkbox
+		    RelativeLayout.LayoutParams checkParams = new RelativeLayout.LayoutParams(
+		    		RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+		    checkParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+		    checkParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+		    this.addView(computer, checkParams);
+		    
+		    //	LayoutParams for the spinner
+		    RelativeLayout.LayoutParams spinnerParams = new RelativeLayout.LayoutParams(
+		    		RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+		    spinnerParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		    spinnerParams.addRule(RelativeLayout.BELOW, computer.getId());
+		    this.addView(selector, spinnerParams);
+		}
+		
+		//	sets whether the selector should be active or not
+		//	enables or disables it only if there is a change from the current state
+		public void setSelector(boolean active) {
+			if (active != selectorActive) {
+				selectorActive = active;
+				if (selectorActive) enableSelector();
+				else disableSelector();
+			}
+		}
+		public void disableSelector() {
+			this.removeView(selector);
+			label.setLayoutParams(selOffLabelParams());
+		}
+		public void enableSelector() {
+			label.setLayoutParams(selOnLabelParams());
+			
+			RelativeLayout.LayoutParams spinnerParams = new RelativeLayout.LayoutParams(
+		    		RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+		    spinnerParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		    spinnerParams.addRule(RelativeLayout.BELOW, computer.getId());
+		    this.addView(selector, spinnerParams);
+		}
+	}
+	
+	private int numPlayers;
+	private PlayerEntry[] players;
+	
+	public GameSetupView(Context context) {
 		super(context);
-		init(context, parent);
+		init(context);
+	}
+	
+	public GameSetupView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(context);
 	}
 	
 	//	initialize the structure
-	void init(Context context, MainMenu parent) {
+	void init(Context context) {
 		this.context = context;
-		this.parent = parent;
     	setupLayout();
-    	
+    	setNumPlayers(1);
     	setFocusable(true);
 	}
 	
 	//	set up the layout
 	void setupLayout() {
-		relShell = new RelativeLayout(context);
-    	RelativeLayout.LayoutParams shellParams = new RelativeLayout.LayoutParams(
-    			RelativeLayout.LayoutParams.MATCH_PARENT,
-    			RelativeLayout.LayoutParams.MATCH_PARENT);
-    	shellParams.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
-    	
-    	relLayout = new RelativeLayout(context);
-    	RelativeLayout.LayoutParams relParams = new RelativeLayout.LayoutParams(
-    			RelativeLayout.LayoutParams.WRAP_CONTENT,
-    			RelativeLayout.LayoutParams.WRAP_CONTENT);
-    	relParams.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
-    	
-    	buttonContainer = new LinearLayout(context);
-    	buttonContainer.setOrientation(LinearLayout.VERTICAL);
-    	LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
-    	
-    	relLayout.addView(buttonContainer, params);
-    	relShell.addView(relLayout, relParams);
-    	addView(relShell, shellParams);
+		this.setOrientation(LinearLayout.VERTICAL);
+		players = new PlayerEntry[4];
+		players[0] = new PlayerEntry(1);
+		players[1] = new PlayerEntry(2);
+		players[2] = new PlayerEntry(3);
+		players[3] = new PlayerEntry(4);
+	}
+	
+	public void setNumPlayers(int num) {
+		if (num <= 4 && num >= 0) {
+			numPlayers = num;
+			this.removeAllViews();
+			for (int i = 0; i < numPlayers; i++) {
+				this.addView(players[i], new LinearLayout.LayoutParams(
+						LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+			}
+		}
+	}
+	
+	public void setChoice (boolean canChoose) {
+		for (int i = 0; i < players.length; i++) {
+			players[i].setSelector(canChoose);
+		}
+	}
+	
+	
+	///
+	///		Utility Methods
+	///
+	
+	private ArrayList<Pair<String,CharacterCard>> getCharacterCards() {
+		ArrayList<Pair<String,CharacterCard>> data = new ArrayList<Pair<String,CharacterCard>>();
+		for (int i = 0; i < CardData.Characters.length; i++) {
+			CharacterCard temp = CardData.Characters[i];
+			data.add(new Pair<String, CharacterCard>(String.format("%s (%s)", temp.getName(), CardData.expansString(temp.getExpansion())), temp));
+		}
+		return data;
+	}
+	
+	private String[] getNameList(ArrayList<Pair<String,CharacterCard>> pairs) {
+		String[] data = new String[pairs.size()];
+		for (int i = 0; i < pairs.size(); i++) {
+			data[i] = pairs.get(i).first;
+		}
+		return data;
 	}
 }
